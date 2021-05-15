e8e390a261d1c9866c44225ad053216b273385fe
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-05-15 15:54:50 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+import signal
+from contextlib import suppress
+import logging
+import sys
+
+from media_devices.audio import Audio
+
+def setup_logger():
+  root = logging.getLogger()
+  root.setLevel(logging.DEBUG)
+  
+  handler = logging.StreamHandler(sys.stdout)
+  handler.setLevel(logging.DEBUG)
+  fmt = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+  handler.setFormatter(fmt)
+  root.addHandler(handler)
+
+async def main():
+    setup_logger()
+
+    audio = Audio()
+    listen_task = asyncio.create_task(audio.listen())
+
+    # register signal handlers to cancel listener when program is asked to terminate
+    for sig in (signal.SIGTERM, signal.SIGHUP, signal.SIGINT):
+        loop.add_signal_handler(sig, listen_task.cancel)
+    # Alternatively, the PulseAudio event subscription can be ended by breaking/returning from the `async for` loop
+
+    with suppress(asyncio.CancelledError):
+        await listen_task
+
+if __name__ == "__main__":
+  loop = asyncio.get_event_loop()
+  loop.run_until_complete(main())
---
+++ media_devices/audio.py
@@ -0,0 +1,32 @@
+import pulsectl_asyncio
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
+class Audio:
+  APP_NAME = 'event-listener'
+
+  def __init__(self):
+    self.sources = {}
+
+  async def listen(self):
+    async with pulsectl_asyncio.PulseAsync(self.APP_NAME) as pulse:
+      await self._get_sources(pulse)
+      async for event in pulse.subscribe_events('source_output'):
+        if event.t not in ["new", "remove"]:
+          continue
+
+        if event.t == "new":
+          source = await pulse.source_output_info(event.index)
+          self.sources[event.index] = source
+          _LOGGER.debug("Started listening: %s", source.proplist['application.name'])
+        
+        if event.t == "remove":
+          source = self.sources[event.index]
+          del self.sources[event.index]
+          _LOGGER.debug("Stopped listening: %s", source.proplist['application.name'])
+
+  async def _get_sources(self, pulse):
+    sources = await pulse.source_output_list()
+    for source in sources:
+      self.sources[source.index] = source
